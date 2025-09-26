253692769ce124db8b09a6e34ea5ead3a946a4b4
James Conroy-Finn <james@invetica.co.uk>
2025-09-26 15:03:40 +0100

    Bootstrap Astro apps

--- devenv.nix
+++ devenv.nix
@@ -12,4 +12,23 @@
     taplo
     treefmt
   ];
+
+  languages.javascript.enable = true;
+  languages.javascript.pnpm.enable = true;
+
+  processes.edit.exec = "pnpm dev:edit";
+  processes.page.exec = "pnpm dev:page";
+
+  process.managers.process-compose.settings.processes = {
+    edit = {
+      environment = [
+        "PORT=3000"
+      ];
+    };
+    page = {
+      environment = [
+        "PORT=3030"
+      ];
+    };
+  };
 }
