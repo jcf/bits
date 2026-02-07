d34e7ea8f2d8cba8ad28673c0cf67b2f67d2be94
James Conroy-Finn <james@invetica.co.uk>
2026-02-07 00:35:43 +0000

    Demo server push

--- dev/bits/dev.clj
+++ dev/bits/dev.clj
@@ -1,11 +1,23 @@
 (ns bits.dev
   (:require
    [bits.app :as app]
+   [bits.next :as next]
    [bits.service :as service]
-   [com.stuartsierra.component.repl :refer [set-init]]
+   [clojure.core.async :as a]
+   [com.stuartsierra.component.repl :refer [set-init system]]
    [hato.client :as http]))
 
 (set-init (fn [_system] (app/system)))
 
 (comment
-  (com.stuartsierra.component.repl/reset))
+  (com.stuartsierra.component.repl/reset)
+
+  (def channels (deref (:channels (:service system))))
+  (def send! (:send! (-> channels vals first)))
+
+  (send! (next/title-event "Hello from the server!"))
+  (send! (next/title-event "Bits"))
+
+  (do
+    (swap! next/!state #(update % :count * 2))
+    (a/put! (:refresh-ch (:service system)) :action)))
