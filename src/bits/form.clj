995755796980f7fc72fce3b8829d69ea48167586
James Conroy-Finn <james@invetica.co.uk>
2026-02-08 18:06:21 +0000

    Add bits.form namespace

---
+++ src/bits/form.clj
@@ -0,0 +1,30 @@
+(ns bits.form
+  (:require
+   [bits.string :as string]
+   [bits.tailwind :as tw]))
+
+;;; ----------------------------------------------------------------------------
+;;; Form generation
+
+(def ^:private default-attrs
+  {:method "post"
+   :action "/action"
+   :class  ["transition-opacity" "inert:opacity-50" "inert:cursor-wait"]})
+
+(defn form
+  [request action-kw & body]
+  (let [[opts children] (tw/normalize-hiccup body)
+        csrf            (:bits.next/csrf request)
+        attrs           (-> default-attrs
+                            (update :class tw/merge-classes (:class opts))
+                            (merge (dissoc opts :class)))]
+    (into [:form attrs
+           [:input {:type "hidden" :name "action" :value (string/keyword->string action-kw)}]
+           [:input {:type "hidden" :name "csrf" :value csrf}]]
+          children)))
+
+(defn action-button
+  [action-kw & body]
+  (let [[opts children] (tw/normalize-hiccup body)]
+    (into [:button (assoc opts :type "button" :data-action (string/keyword->string action-kw))]
+          children)))
