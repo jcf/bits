d53be562f26a94c9a5bc6c30352e00f20753a704
James Conroy-Finn <james@invetica.co.uk>
2026-02-08 18:18:35 +0000

    Configure bits.form indentation

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,9 @@
 ;;; Directory Local Variables         -*- no-byte-compile: t; -*-
 ((clojure-mode
-  . ((cider-ns-refresh-after-fn . "bits.dev/after-refresh")
+  . ((eval . (define-clojure-indent
+               (action-button 1)
+               (form 2)))
+     (cider-ns-refresh-after-fn . "bits.dev/after-refresh")
      (cider-ns-refresh-before-fn . "bits.dev/before-refresh")
      (cider-clojure-cli-aliases . ":dev:test")
      (cider-redirect-server-output-to-repl . nil)
