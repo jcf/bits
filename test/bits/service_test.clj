097f5e9dfbb50de16e2e3fe5c224738d4caddf63
James Conroy-Finn <james@invetica.co.uk>
2026-02-10 00:28:32 +0000

    Add CSRF protection tests
    Verify valid token passes and missing token returns 403.

--- test/bits/service_test.clj
+++ test/bits/service_test.clj
@@ -5,6 +5,9 @@
    [clojure.test :refer [deftest is]]
    [matcher-combinators.test]))
 
+;;; ----------------------------------------------------------------------------
+;;; Secure headers
+
 (defn csp
   [nonce]
   (str/join "; "
@@ -21,7 +24,7 @@
       (let [request {:request-method :get
                      :url            "/"}]
         (is (match?
-             {"content-security-policy"           csp
+             {"content-security-policy"           (csp "YWJj")
               "referrer-policy"                   "strict-origin"
               "server"                            "Bits"
               "strict-transport-security"         "max-age=31536000; includeSubdomains"
@@ -31,3 +34,43 @@
               "x-permitted-cross-domain-policies" "none"
               "x-xss-protection"                  "1; mode=block"}
              (:headers (t/request service request))))))))
+
+;;; ----------------------------------------------------------------------------
+;;; CSRF
+
+(defn- extract-csrf-token
+  [response]
+  (let [cookies (get-in response [:headers "set-cookie"])]
+    (some->> (if (string? cookies) [cookies] cookies)
+             (filter #(str/starts-with? % "bits-csrf="))
+             first
+             (re-find #"bits-csrf=([^;]+)")
+             second)))
+
+(deftest csrf
+  (t/with-system [{:keys [service]} (t/system)]
+    (let [http-client (t/http-client {:cookie-handler (t/cookie-manager)})
+          request     {:http-client    http-client
+                       :request-method :get
+                       :url            "/"}
+          response    (t/request service request)
+          token       (extract-csrf-token response)]
+
+      (when (is (string? token)
+                (str "Token: " (pr-str token)))
+        (is (match?
+             {:status 200}
+             (t/request service
+                        {:http-client http-client
+                         :method      :post
+                         :url         "/action"
+                         :form-params {:csrf   token
+                                       :action "auth/sign-out"}})))
+
+        (is (match?
+             {:status 403}
+             (t/request service
+                        {:http-client http-client
+                         :method      :post
+                         :url         "/action"
+                         :form-params {:action "auth/sign-out"}})))))))
