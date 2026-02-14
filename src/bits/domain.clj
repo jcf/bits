3bafbbc599fafb5c9ca7b0ad5b47374f2981be7e
James Conroy-Finn <james@invetica.co.uk>
2026-02-14 15:15:50 +0000

    Remove dead code from old Pedestal service

--- src/bits/domain.clj
+++
@@ -1,8 +0,0 @@
-(ns bits.domain
-  (:require
-   [clojure.spec.alpha :as s]))
-
-;; TODO Combine with bits.schema.
-(s/def :user/id uuid?)
-(s/def :user/created-at inst?)
-(s/def :user/password-hash string?)
