0336854f01ec19cfeb881e0de6c5688b5f83ad7f
James Conroy-Finn <james@invetica.co.uk>
2026-02-20 17:55:32 +0000

    Add body-style indentation for UI component functions

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,14 @@
 ;;; Directory Local Variables         -*- no-byte-compile: t; -*-
 ((clojure-mode
-  . ((eval . (define-clojure-indent
-               (action-button 1)
-               (form 2)))
-     (cider-ns-refresh-after-fn . "bits.dev/after-refresh")
+  . ((cider-ns-refresh-after-fn . "bits.dev/after-refresh")
      (cider-ns-refresh-before-fn . "bits.dev/before-refresh")
      (cider-clojure-cli-aliases . ":dev:test")
      (cider-redirect-server-output-to-repl . nil)
-     (cider-preferred-build-tool . clojure-cli))))
+     (cider-preferred-build-tool . clojure-cli)
+     (eval . (define-clojure-indent
+              (action-button 1)
+              (card 1)
+              (form 2)
+              (page-center 1)
+              (page-title 1)
+              (text-muted 1))))))
