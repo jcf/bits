661022d29c14dc7b7ac3157a51bc362e274d6a51
James Conroy-Finn <james@invetica.co.uk>
2026-03-04 18:42:05 +0000

    Add test suite and namespace tags to telemetry spans
    Enables filtering test traces in Jaeger by test.suite (e2e, unit,
    generative) or test.namespace.

--- test/bits/test/telemetry.clj
+++ test/bits/test/telemetry.clj
@@ -4,6 +4,13 @@
    [io.pedestal.log :as log]
    [steffan-westcott.clj-otel.api.trace.span :as span]))
 
+(defn- test-suite
+  [m]
+  (cond
+    (:generative m) "generative"
+    (:e2e m)        "e2e"
+    :else           "unit"))
+
 (defn install!
   []
   (when-not (::instrumented? (meta clojure.test/test-var))
@@ -12,10 +19,13 @@
                       (constantly
                        (with-meta
                          (fn [v]
-                           (let [m         (meta v)
-                                 test-name (str (:ns m) "/" (:name m))]
+                           (let [m              (meta v)
+                                 test-name      (str (:ns m) "/" (:name m))
+                                 test-namespace (str (:ns m))]
                              (span/with-span! {:name       test-name
-                                               :attributes {"test.name" test-name}}
+                                               :attributes {"test.name"      test-name
+                                                            "test.namespace" test-namespace
+                                                            "test.suite"     (test-suite m)}}
                                (original v))))
                          {::instrumented? true
                           ::original      original})))
