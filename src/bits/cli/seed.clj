ca9ace76618d18cef0372d1d3bded86d2d214a07
James Conroy-Finn <james@invetica.co.uk>
2026-04-13 18:38:27 +0100

    Wire up seed CLI command

--- src/bits/cli/seed.clj
+++ src/bits/cli/seed.clj
@@ -1,11 +1,18 @@
-(ns bits.cli.seed)
+(ns bits.cli.seed
+  (:require
+   [bits.datomic :as datomic]
+   [bits.dev.realm :as realm]
+   [datomic.api :as d]
+   [java-time.api :as time]))
 
 (def spec
   {})
 
 (defn run
-  [_datomic _ctx]
-  (println "Seeding coming soon!"))
+  [datomic _ctx]
+  (let [seeder (realm/make-seeder (time/java-date))]
+    @(d/transact (datomic/conn datomic) (realm/seed-txes seeder))
+    (println "Seed data applied.")))
 
 (def command
   {:component :datomic
